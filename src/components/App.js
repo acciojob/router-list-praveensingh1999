f98658829aaa020710d15b7d8c5cf4ac97cf5156
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-10-06 22:43:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,26 @@ import React from 'react';
 import Page1 from './Page1';
 import Page2 from './Page2';
 import Page3 from './Page3';
-import { Route, Routes, Link } from 'react-router-dom';
-import './../styles/App.css';
+import { Route, Routes, Link, useLocation } from 'react-router-dom';
 
 const App = () => {
+  const location = useLocation();
+
+  // Show the list only on the main page
+  const showList = location.pathname === '/';
+
   return (
     <div>
-      <h1>List Items</h1>
-      <ul>
-        <li>
-          <Link to='/items/1'>Item 1</Link>
-        </li>
-        <li>
-          <Link to='/items/2'>Item 2</Link>
-        </li>
-        <li>
-          <Link to='/items/3'>Item 3</Link>
-        </li>
-      </ul>
+      {showList && (
+        <>
+          <h1>List Items</h1>
+          <ul>
+            <li><Link to='/items/1'>Item 1</Link></li>
+            <li><Link to='/items/2'>Item 2</Link></li>
+            <li><Link to='/items/3'>Item 3</Link></li>
+          </ul>
+        </>
+      )}
 
       <Routes>
         <Route path='/items/1' element={<Page1 />} />
