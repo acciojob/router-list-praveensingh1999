0c5fbf625116b42748a1fdb11500363c84998ca4
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-10-06 20:37:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,33 @@
-
-import React from "react";
-import Page1 from './Page1'
-import Page2 from './Page2'
-import Page3 from './Page3'
-import { Route,Routes } from 'react-router-dom'
+import React from 'react';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import Page3 from './Page3';
+import { Route, Routes, Link } from 'react-router-dom';
 import './../styles/App.css';
-import { Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-        <h1>List Items</h1>
-    <ul>
-        <li> <a href="/items/:1">item 1</a></li>
-         <li> <a href="/items/:2">item 2</a></li>
-          <li> <a href="/items/:3">item 3</a></li>
-    </ul>
-         
-   
-    
+      <h1>List Items</h1>
+      <ul>
+        <li>
+          <Link to='/items/1'>Item 1</Link>
+        </li>
+        <li>
+          <Link to='/items/2'>Item 2</Link>
+        </li>
+        <li>
+          <Link to='/items/3'>Item 3</Link>
+        </li>
+      </ul>
 
-    <Routes>
-        <Route path='/items/:1' element={<Page1/>}/>
-         <Route path='/items/:2' element={<Page2/>}/>
-         <Route path='/items/:3' element={<Page3/>}/>
-         
-         <Route path='*' element={<Error/>}></Route>
-       </Routes> 
+      <Routes>
+        <Route path='/items/1' element={<Page1 />} />
+        <Route path='/items/2' element={<Page2 />} />
+        <Route path='/items/3' element={<Page3 />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
